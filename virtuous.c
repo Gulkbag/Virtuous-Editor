0fe45cde86f7a4b8104bc7f3f7f7341d4a94e805
Gulkbag <49290305+Gulkbag@users.noreply.github.com>
2019-07-01 22:41:48 +0200

    Update virtuous.c

--- virtuous.c
+++ virtuous.c
@@ -4,9 +4,8 @@
  *
  * -----------------------------------------------------------------------
  *
- * Copyright (C) 2019 Ahmed nasser <antirez at gmail dot com>
+ * Copyleft (C) 2019 Ahmed nasser <antirez at gmail dot com>
  *
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
